8989faaf34d0347b6a30e30b2641f7248ea29f91
Ivan Rao <ivan@Ivans-Mac-Mini.local>
2023-04-13 00:27:40 -0400

    added router for sheets

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 // Load each controller
 const authController = require("./auth.js");
+const sheetController = require("./sheet.js");
 
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
 router.use("/auth", authController);
+router.use("/sheet", sheetController);
 
 module.exports = router;
